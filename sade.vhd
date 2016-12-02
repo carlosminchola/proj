0e0ea1fdf4467bcefc9e5a0430eed365db03c5a1
carlos <cemg228@gmail.com>
2016-12-02 01:11:45 +0100

    sade8

--- sade.vhd
+++ sade.vhd
@@ -1,7 +1,7 @@
 adv slibrary ieee;
 -- use ieee.stdcvfvfv_logic_1164.all;
 use ieee.std_  g gogic_arith.all;
-use ieee.std_logic_unsigned.all;
+use ieee.std_logic_unsigned.all;bbbbb
 use work.crf158_sade_pkg.all;
 btfnfnf
 entity sade_TB is dvfb
