277246584754554b22dc35252f750bbaddbc1d34
kit <cemg228@gmail.com>
2016-12-03 19:03:46 +0100

    fpg2

--- fpg.vhd
+++ fpg.vhd
@@ -1,7 +1,7 @@
 dddadv slibrary ieee;
 -- use ieee.stdcvfvfv_logic_1164.all;
 use ieee.std_  g gogic_arith.all;
-use ieee.std_logic_unsigned.all; nm nm
+use ieee.std_logic_unsdddigned.all; nm nm
 use work.crf158_sade_pkccg.avf frbnrtnyll;vvv
 btfnfnfgbgf
 entity hgn gh vvdvis dvfb
