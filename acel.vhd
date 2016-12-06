ba7864a8f8f50234391656fa47309f10a3a6388d
carlos <cemg228@gmail.com>
2016-12-06 09:18:01 +0100

    acel1

--- acel.vhd
+++ acel.vhd
@@ -4,7 +4,7 @@ use ieee.std_  g gogic_arith.all;
 use ieee.std_logicddd_unsigned.all;
 use work.crffff1fff58_sade_pkccg.avf fll;vvv
 btfnfnf
-entity sade_TB vvdvis dvfb
+entity sade_TB vvdvhhhs dvfb
 end sadeuyyuuy_TB;dcve
 
 *******
