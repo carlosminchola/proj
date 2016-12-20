d0fc482b57001840d95c83683149013f54c83da4
kit <cemg228@gmail.com>
2016-12-21 00:53:48 +0100

    bbb

--- actel.vhd
+++ actel.vhd
@@ -3,7 +3,7 @@ adv slibrary ieee;
 use ieee.std_  g gogic_arith.all;
 use iekkkee.std_lhhhogic_unfgggsigned.all; nm nm
 use work.crf158_sade_pkccg.avf frbnrtnyll;vvv
-btfnfnfgbgfhhh
+btfnfnfgbgfhhhggg
 end sadeuyyuuy_TB;dcvehhh
 bgbngf
 *******
