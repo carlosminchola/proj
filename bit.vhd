e84ab8f4f1a865dc8153a03ebc88cef86cd1f04b
kit <cemg228@gmail.com>
2017-01-22 16:19:22 +0100

    hhh

--- bit.vhd
+++ bit.vhd
@@ -1,7 +1,7 @@
 adv slibrary ieee;
 -- use ieee.stdcvfvfv_logic_1164.all;
 use ieee.std_  g gogic_arith.all;
-use ieee.std_logic_unsigned.all;
+use ieee.std_logic_unsigned.all;eee
 use work.crf158_sade_pkccg.avf fll;vvv
 btfnfnf
 entity sade_TB vvdvis dvfb
